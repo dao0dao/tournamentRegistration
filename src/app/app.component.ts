eadb98922f4f2aa5305572fc129939d274ee77c9
Demid Greshnikov <d_greshnikov@tlen.pl>
2020-12-15 22:54:11 +0100

    change colors in scss files and add heading in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('title', [
+      transition('void => *', [style({ opacity: 0 }), animate('200ms ease-in')]),
+      transition('* => void', [animate('200ms ease-out', style({ opacity: 0 }))])
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
 
+  showTitle: boolean = false
 
-  constructor(public autService: AuthorizationService) { }
+  constructor(public autService: AuthorizationService, private router: Router) { }
+  ngDoCheck() {
+    if (this.router.url !== '/') { this.showTitle = true } else { this.showTitle = false }
+  }
 }
